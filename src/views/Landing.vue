0d61a1a82d13ab1b5a91bcca93e73e72dd3b0625
Richardus Ferdian <ferdianditanugraha@gmail.com>
2021-11-18 20:50:09 +0700

    first commit

---
+++ src/views/Landing.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="landing">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Landing',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.landing{
+  background-image: url("../assets/landing-bg.png");
+}
+</style>
