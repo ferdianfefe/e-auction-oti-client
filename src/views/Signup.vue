265f853a65cef823ac16bf7d99770abc67ae4a6b
NoodleReza <38813206+anoodleReza@users.noreply.github.com>
2021-11-30 23:07:42 +0700

    Styling for sign up
    Matches with sign-in page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,60 +1,91 @@
 <template>
-  <div class="landing h-100">
-    <div class="container">
-      <h4 class="text-white align-left my-5">Sign Up</h4>
-      <div class="row">
-        <form class="col-6" @submit.prevent="submitSignup">
-          <div class="form-group">
-            <label for="name" class="text-white align-left">Name</label>
-            <input
-              type="text"
-              class="form-control"
-              id="name"
-              placeholder="Enter name"
-              v-model="user.name"
-            />
+  <body class="u-body">
+    <section class="u-align-center u-clearfix u-grey-5 u-section-1" id="sec-d516" >
+      <br><br>
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1" >
+        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+
+              <div class="u-align-left u-container-style u-image u-layout-cell u-size-31 u-image-1" data-image-width="1600" data-image-height="900">
+                <div class="u-container-layout u-container-layout-1"></div>
+              </div>
+
+              <div class="u-align-center-sm u-align-center-xs u-container-style u-layout-cell u-shape-rectangle u-size-29 u-white u-layout-cell-2">
+                
+                <div class="u-container-layout u-container-layout-2">
+                  <br><br>
+                  <h3 class="u-align-center-lg u-align-center-md u-align-center-xl u-text u-text-default u-text-1">Create Your Gamma Account.</h3>
+                  <div class="u-expanded-width-xs u-form u-login-control u-form-1">
+
+                    <form @submit.prevent="submitSignup" class="u-clearfix u-form-custom-backend u-form-spacing-15 u-form-vertical u-inner-form" source="custom" name="form-3" style="padding: 0px;">
+                      
+                      <div class="u-form-group u-form-name">
+                        <label for="name" class="u-form-control-hidden u-label"></label>
+                        <input
+                          type="text"
+                          class="u-border-2 u-border-black u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle"
+                          id="name"
+                          placeholder="Enter name"
+                          v-model="user.name"
+                        />
+                      </div>
+                      <div class="u-form-group u-form-username">
+                        <label for="username" class="u-form-control-hidden u-label"></label>
+                        <input
+                          type="text"
+                          class="u-border-2 u-border-black u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle"
+                          id="username"
+                          placeholder="Enter username"
+                          v-model="user.username"
+                        />
+
+                      </div>
+                      <div class="u-form-group u-form-password">
+                        <label for="password" class="u-form-control-hidden u-label"></label>
+                        <input
+                          type="password"
+                          class="u-border-2 u-border-black u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle"
+                          id="password"
+                          placeholder="Enter password"
+                          v-model="user.password"
+                        />
+                      </div>
+                      <div class="u-form-group u-form-repassword">
+                        <label for="password" class="u-form-control-hidden u-label"></label>
+                        <input
+                          type="password"
+                          class="u-border-2 u-border-black u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle"
+                          id="confirm-password"
+                          placeholder="Repeat password"
+                          v-model="user.confirmPassword"
+                        />
+                      </div>
+
+                      <div class="u-align-center u-form-group u-form-submit">
+                        <br> 
+                        <button type="submit" 
+                          class="u-active-palette-1-light-1 u-border-none u-btn u-btn-round u-btn-submit u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-50 u-text-body-alt-color u-btn-1">
+                          Sign Up</button>
+                      </div>
+                      <input type="hidden" value="" name="recaptchaResponse">
+                    </form>
+
+                    <a @click="$router.push('/signin')" class="u-border-1 u-border-active-palette-1-base u-border-hover-palette-1-base u-btn u-button-style u-login-control u-login-forgot-password u-none u-text-palette-1-base u-btn-2" >
+                      Already have an account?
+                    </a>
+
+                  </div>
+                  <br><br><br>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="form-group mt-4">
-            <label for="username" class="text-white align-left">Username</label>
-            <input
-              type="text"
-              class="form-control"
-              id="username"
-              placeholder="Enter username"
-              v-model="user.username"
-            />
-          </div>
-          <div class="form-group mt-4">
-            <label for="password" class="text-white align-left">Password</label>
-            <input
-              type="password"
-              class="form-control"
-              id="password"
-              placeholder="Enter password"
-              v-model="user.password"
-            />
-          </div>
-          <div class="form-group mt-4">
-            <label for="password" class="text-white align-left"
-              >Confirm Password</label
-            >
-            <input
-              type="password"
-              class="form-control"
-              id="confirm-password"
-              placeholder="Repeat password"
-              v-model="user.confirmPassword"
-            />
-          </div>
-          <button type="submit" class="btn btn-primary mt-3">Sign Up</button>
-          <small class="text-white"
-            >Already had an account?
-            <router-link to="/" class="text-white">Sign in</router-link></small
-          >
-        </form>
+        </div>
       </div>
-    </div>
-  </div>
+      <br><br><br>
+    </section>
+  </body>
 </template>
 
 <script>
@@ -101,30 +132,19 @@ export default {
         password: this.user.password,
       };
 
-      this.signup(userData);
+      this.signup(userData).then((res) => {
+        if (res) {
+          if (res.success) {
+            this.$router.push("/about");
+          }
+        }
+      });
+
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.landing {
-  margin: 0;
-  padding: 0;
 
-  min-height: 100vh;
-  background-position: bottom;
-  transition: 0.4s;
-
-  background: linear-gradient(
-      180deg,
-      rgba(75, 24, 24, 0.58) 0%,
-      rgba(3, 13, 52, 0.5) 68.75%,
-      #141341 97.4%
-    ),
-    url("../assets/landing-bg.png");
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  background-size: 100%;
-  background-repeat: no-repeat;
-}
 </style>
