947bab05da4b2dbddc26aa212a6882e6a71bf355
NoodleReza <38813206+anoodleReza@users.noreply.github.com>
2021-12-04 20:26:14 +0700

    Copied styles from homepage

--- src/views/MyAuctions.vue
+++ src/views/MyAuctions.vue
@@ -54,15 +54,95 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.my-auctions {
-  min-height: 100vh;
-}
-body {
+/* flexbox start */
+.row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+  align-content: flex-start;
+
+  min-width: 100%;
   margin: 0;
   padding: 0;
-  background: white;
 }
-
+.card {
+  margin: 10px;
+  padding: 1rem;
+  background: none;
+  border: none;
+  box-shadow: none;
+  min-width: 300px;
+}
+.card img {
+  width: 15rem;
+  height: 15.5rem;
+  object-fit: cover;
+  margin-left: 1rem;
+}
+.card-body {
+  padding-top: 0;
+}
+.card-body button {
+  background: none;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  color: black;
+  border-radius: 0%;
+  width: 10rem;
+}
+// .d-flex {
+//   background: red;
+// }
+/* flexbox end */
+@media (max-width: 800px) {
+  .row {
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 1rem;
+  }
+}
+/* responsive */
+h5,
+p {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 1rem;
+  line-height: 15px;
+}
+p {
+  font-weight: normal;
+  font-size: 0.75rem;
+}
+/* top panel */
+.top-bar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 2rem;
+}
+.textbox {
+  margin: 0;
+  padding: 0;
+}
+.line {
+  width: 7rem;
+  height: 4px;
+  left: 183px;
+  top: 650px;
+  background: #bc9b33;
+}
+.new-auc {
+  float: right;
+}
+h4 {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 1.25rem;
+  line-height: 24px;
+}
 #placeholder {
   width: 100%;
   height: auto;
